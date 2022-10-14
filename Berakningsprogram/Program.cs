d63d0ee7128c949863c0aaeed84e3776f34ffe42
Tomas Kindahl <tomas.kindahl@bahnhof.se>
2022-10-14 03:11:33 +0200

    Add project files.

---
+++ Berakningsprogram/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Berakningsprogram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hej och välkommen till beräkningsprogrammet.");
+            Console.WriteLine("Skriv 'hjälp' för hjälp!");
+            string command;
+            do
+            {
+                Console.Write("Kommando: ");
+                command = Console.ReadLine();
+                if (command == "plus")
+                {
+                    Console.Write("tal 1: ");
+                    double X = double.Parse(Console.ReadLine());
+                    Console.Write("tal 2: ");
+                    double Y = double.Parse(Console.ReadLine());
+                    Console.WriteLine($"{X} + {Y} = {X + Y}");
+                }
+                else if (command == "hjälp")
+                {
+                    Console.WriteLine($"Tyvärr ej implementerat!");
+                }
+                else if (command == "sluta")
+                {
+
+                }
+                else
+                {
+                    Console.WriteLine($"Okänt kommando: {command}");
+                }
+            } while (command != "sluta");
+            Console.WriteLine("Hej då!");
+        }
+    }
+}
